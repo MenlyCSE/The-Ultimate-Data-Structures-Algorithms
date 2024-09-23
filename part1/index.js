b82fb2cdd609eec22bfecea1fcd0bcbd29076787
MenlyCSE <MenlyCSE@gmail.com>
2024-09-22 20:17:10 -0400

    Update index.js

--- part1/index.js
+++ part1/index.js
@@ -1,1 +1,15 @@
-console.log('Hey there!')
+// 
+class Main {
+    // 0(n + m)
+    log(numbers, names) {
+        // 0(n) - int
+        for (const number of numbers) 
+            console.log(number);
+
+        // 0(m) - string
+        for (const name of names)
+            console.log(name);
+    }
+}
+
+const m = new Main();
