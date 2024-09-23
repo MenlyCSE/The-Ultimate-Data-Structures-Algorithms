c44ed2d232be69ef022c36aad5e7c8b01a38ff8b
MenlyCSE <MenlyCSE@gmail.com>
2024-09-23 19:01:24 -0400

    array exercise

--- part1/index.js
+++ part1/index.js
@@ -1,14 +1,38 @@
-// space complexity
-class Main {
-    array() {
-        //
-        const numbers = [];
-        numbers.push(10);
-        numbers.push(20);
-        numbers.push(30);
-        numbers.push(40);
-        console.log(numbers);
+// 
+const _array = new WeakMap();
+
+class Array {
+    constructor() {
+        _array.set(this, []);
+    }
+    
+    show() {
+        return _array.get(this);
+    }
+
+    indexOf(number) {
+        let i = 0;
+        while (number !== _array.get(this)[i] && i < _array.get(this).length)
+            i++;
+
+        if (number === _array.get(this)[i])
+            return `found at index ${i}`;
+
+        return `index -1`;
+    }
+
+    insert(obj) {
+        _array.get(this).push(obj);
+        return `pushed ${obj}`;
+    }
+
+    removeAt(index) {
+        _array.get(this)[index] = `removed`;
+        return `removed at index ${index}`;
     }
 }
 
-const m = new Main();
+const a = new Array();
+
+for (let i = 7; i <= 12; i++)
+    a.insert(i);
