b12077088e8ff76fb5592f37963682f0c4565797
MenlyCSE <MenlyCSE@gmail.com>
2024-09-25 17:44:54 -0400

    linked lists

--- part1/index.js
+++ part1/index.js
@@ -1,16 +1,1 @@
-// dynamic array 
-let lists = [];
-
-lists.push(10);
-lists.push(20);
-lists.push(30);
-lists.splice(0, 1);
-
-
-for (item of lists)
-    console.log(item);
-
-console.log(`Index of 20... ${lists.indexOf(20)}`);
-console.log(`Last occurences of 20... ${lists.lastIndexOf(20)}`);
-console.log(`Includes 20... ${lists.includes(20)}`);
-console.log(`Array length... ${lists.length}`);
+// linked lists
