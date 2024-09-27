7841622a351d9cd0fc952c69b935efaa4d225ac0
MenlyCSE <MenlyCSE@gmail.com>
2024-09-26 20:44:06 -0400

    LinkedList

--- part1/index.js
+++ part1/index.js
@@ -1,1 +1,23 @@
 // linked lists
+class Node {
+    constructor() {
+        this.value = [];
+        this.next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.first;
+        this.last;
+    }
+
+    // addFirst
+    // addLast
+    // deleteFirst
+    // delteLast
+    // contains
+    // indexOf
+}
+
+const list = new LinkedList();
